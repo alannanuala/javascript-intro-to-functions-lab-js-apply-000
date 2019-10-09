2c94ad73a589b08a62e6b98e8e4949ccd7a5c3bb
victorious-firmware-2064 <alanna.nuala@gmail.com>
2019-10-09 15:28:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const shout = str => {
 const logShout = str => {
   console.log(str.toUpperCase())
 }
+
+const whipser = str =< {
+  return str.toLowerCase()
+}
+
+const logWhisper = str => {
+  console.log(str.toLowerCase())
+}
